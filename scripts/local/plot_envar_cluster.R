57e178a8c3b7d8405d455cb4db34988d109cbfd1
Ryan Reisinger <ryan.r.reisinger@gmail.com>
2022-04-13 15:19:29 +0100

    Local

--- scripts/local/plot_envar_cluster.R
+++ scripts/local/plot_envar_cluster.R
@@ -7,7 +7,7 @@ library(pals)
 library(colorspace)
 library(gridExtra)
 
-setwd("D:/RAATD/hotspot/")
+setwd("D:/soPredatorRegions")
 
 #-----------------------------------------
 ## Read in gridded data with groups classified
@@ -15,7 +15,7 @@ dat <- readRDS("./dat_out/gridded_with_clusters_quartile.RDS")
 
 #### custom theme
 theme_ryan <- function () { 
-  theme_bw(base_size=9, base_family="") %+replace% 
+  theme_bw(base_size=8, base_family="") %+replace% 
     theme(
       axis.text = element_text(colour = "black"),
       axis.ticks = element_line(colour = "black"),
@@ -44,48 +44,49 @@ dat_long <- dat_long[!is.na(dat_long$group2), ]
 
 # Colours
 ## Creat a colour palette
-k.hot <- length(which(grepl("hot_", unique(dat$group2))))
-k.cold <- length(which(grepl("cold_", unique(dat$group2))))
-pal <- c(sequential_hcl(k.cold, palette = "Light Grays"), glasbey(k.hot))
+# k.hot <- length(which(grepl("hot_", unique(dat$group2))))
+# k.cold <- length(which(grepl("cold_", unique(dat$group2))))
+# pal <- c(sequential_hcl(k.cold, palette = "Light Grays"), glasbey(k.hot))
+pal <- glasbey(17)
 
 # Reorder factors to match clustering
-dat_long$group2 <- factor(dat_long$group2, levels = c("hot_13",
-                                                      "hot_15",
-                                                      "hot_16",
-                                                      "cold_14",
-                                                      "hot_17",
-                                                      "hot_09",
-                                                      "hot_10",
-                                                      "hot_03",
-                                                      "hot_08",
-                                                      "hot_04",
-                                                      "cold_01",
-                                                      "cold_02",
-                                                      "hot_05",
-                                                      "hot_06",
-                                                      "hot_07",
-                                                      "hot_11",
-                                                      "hot_12"))
+dat_long$group2 <- factor(dat_long$group2, levels = c("13",
+                                                      "15",
+                                                      "16",
+                                                      "14",
+                                                      "17",
+                                                      "09",
+                                                      "10",
+                                                      "03",
+                                                      "08",
+                                                      "04",
+                                                      "01",
+                                                      "02",
+                                                      "05",
+                                                      "06",
+                                                      "07",
+                                                      "11",
+                                                      "12"))
 
 # Reorder palette
 # Right order
-ordered_clusters <- c("hot_13",
-                      "hot_15",
-                      "hot_16",
-                      "cold_14",
-                      "hot_17",
-                      "hot_09",
-                      "hot_10",
-                      "hot_03",
-                      "hot_08",
-                      "hot_04",
-                      "cold_01",
-                      "cold_02",
-                      "hot_05",
-                      "hot_06",
-                      "hot_07",
-                      "hot_11",
-                      "hot_12")
+ordered_clusters <- c("13",
+                      "15",
+                      "16",
+                      "14",
+                      "17",
+                      "09",
+                      "10",
+                      "03",
+                      "08",
+                      "04",
+                      "01",
+                      "02",
+                      "05",
+                      "06",
+                      "07",
+                      "11",
+                      "12")
 
 # Match
 dx <- vector(length = length(ordered_clusters))
@@ -94,47 +95,47 @@ dx[i] <- which(unique(as.character(dat_long$group2)) == ordered_clusters[i])
 }
 
 # Reorder the palette
-pal <- pal[c(1,2, 4:14, 3, 15:17)] # reorder to match colours to original order 
+# pal <- pal[c(1,2, 4:14, 3, 15:17)] # reorder to match colours to original order 
 pal <-pal[dx] # reorder by factor order
 
 # Add supercluster
 dat_long <- dat_long %>%
-  mutate(super_group = case_when(group2 == "hot_13" ~ "antarctic",
-                                 group2 == "hot_15" ~ "antarctic",
-                                 group2 == "hot_16" ~ "antarctic",
-                                 group2 == "cold_14" ~ "antarctic",
-                                 group2 == "hot_17" ~ "antarctic",
-                                 group2 == "hot_09" ~ "scotia_arc",
-                                 group2 == "hot_10" ~ "scotia_arc",
-                                 group2 == "hot_03" ~ "subantarctic_distant",
-                                 group2 == "hot_08" ~ "subantarctic_distant",
-                                 group2 == "hot_04" ~ "subantarctic_distant",
-                                 group2 == "cold_01" ~ "subantarctic_distant",
-                                 group2 == "cold_02" ~ "subantarctic_distant",
-                                 group2 == "hot_05" ~ "subantarctic",
-                                 group2 == "hot_06" ~ "subantarctic",
-                                 group2 == "hot_07" ~ "subantarctic",
-                                 group2 == "hot_11" ~ "subantarctic",
-                                 group2 == "hot_12" ~ "subantarctic"))
+  mutate(super_group = case_when(group2 == "13" ~ "antarctic",
+                                 group2 == "15" ~ "antarctic",
+                                 group2 == "16" ~ "antarctic",
+                                 group2 == "14" ~ "antarctic",
+                                 group2 == "17" ~ "antarctic",
+                                 group2 == "09" ~ "scotia_arc",
+                                 group2 == "10" ~ "scotia_arc",
+                                 group2 == "03" ~ "subantarctic_distant",
+                                 group2 == "08" ~ "subantarctic_distant",
+                                 group2 == "04" ~ "subantarctic_distant",
+                                 group2 == "01" ~ "subantarctic_distant",
+                                 group2 == "02" ~ "subantarctic_distant",
+                                 group2 == "05" ~ "subantarctic",
+                                 group2 == "06" ~ "subantarctic",
+                                 group2 == "07" ~ "subantarctic",
+                                 group2 == "11" ~ "subantarctic",
+                                 group2 == "12" ~ "subantarctic"))
 
 dat_long <- dat_long %>%
-  mutate(super_group_ab = case_when(group2 == "hot_13" ~ "ANT",
-                                 group2 == "hot_15" ~ "ANT",
-                                 group2 == "hot_16" ~ "ANT",
-                                 group2 == "cold_14" ~ "ANT",
-                                 group2 == "hot_17" ~ "ANT",
-                                 group2 == "hot_09" ~ "SCO",
-                                 group2 == "hot_10" ~ "SCO",
-                                 group2 == "hot_03" ~ "DIS",
-                                 group2 == "hot_08" ~ "DIS",
-                                 group2 == "hot_04" ~ "DIS",
-                                 group2 == "cold_01" ~ "DIS",
-                                 group2 == "cold_02" ~ "DIS",
-                                 group2 == "hot_05" ~ "SUB",
-                                 group2 == "hot_06" ~ "SUB",
-                                 group2 == "hot_07" ~ "SUB",
-                                 group2 == "hot_11" ~ "SUB",
-                                 group2 == "hot_12" ~ "SUB"))
+  mutate(super_group_ab = case_when(group2 == "13" ~ "ANT",
+                                 group2 == "15" ~ "ANT",
+                                 group2 == "16" ~ "ANT",
+                                 group2 == "14" ~ "ANT",
+                                 group2 == "17" ~ "ANT",
+                                 group2 == "09" ~ "SCO",
+                                 group2 == "10" ~ "SCO",
+                                 group2 == "03" ~ "DIS",
+                                 group2 == "08" ~ "DIS",
+                                 group2 == "04" ~ "DIS",
+                                 group2 == "01" ~ "DIS",
+                                 group2 == "02" ~ "DIS",
+                                 group2 == "05" ~ "SUB",
+                                 group2 == "06" ~ "SUB",
+                                 group2 == "07" ~ "SUB",
+                                 group2 == "11" ~ "SUB",
+                                 group2 == "12" ~ "SUB"))
 
 # Plot in a list
 plist <- list()
